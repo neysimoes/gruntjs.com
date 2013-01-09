225d3e0383135e37547ddb041e022a39d8b0ebcc
Sindre Sorhus <sindresorhus@gmail.com>
2013-01-09 03:18:49 +0100

    Code style improvements and convert to 2 space indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,42 @@
 'use strict';
-
-module.exports = function(grunt) {
-
-  // Project configuration.
+module.exports = function (grunt) {
   grunt.initConfig({
     jshint: {
       files: ['js/*.js']
     },
     concat: {
-      main: {
-        dest: 'dist/main.js',
-        src: [
-          'js/vendor/jquery.js',
-          'js/vendor/lodash.js',
-          'js/vendor/jquery.timeago.js',
-          'js/vendor/list.js',
-          'js/main.js',
-        ]
+      dist: {
+        files: {
+          'dist/main.js': [
+            'js/vendor/jquery.js',
+            'js/vendor/lodash.js',
+            'js/vendor/jquery.timeago.js',
+            'js/vendor/list.js',
+            'js/main.js',
+          ]
+        }
       }
     },
     uglify: {
-      main: {
-        dest: 'dist/main.js',
-        src: 'dist/main.js'
+      dist: {
+        files: {
+          'dist/main.js': 'dist/main.js'
+        }
       }
     },
   });
 
-  // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('build-redirects', 'Build redirect html files.', function() {
+  grunt.registerTask('build-redirects', 'Build redirect html files.', function () {
     var redirects = grunt.file.readYAML('redirects.yml');
-    Object.keys(redirects).forEach(function(path) {
+    Object.keys(redirects).forEach(function (path) {
       var url = redirects[path];
       grunt.log.write('Creating redirect for gruntjs.com/' + path + '...');
       grunt.file.copy('tmpl/redirect.tmpl', path + '/index.html', {
-        process: function(src) {
+        process: function (src) {
           return grunt.template.process(src, {data: {url: url}});
         }
       });
@@ -46,7 +44,10 @@ module.exports = function(grunt) {
     });
   });
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'build-redirects']);
-
+  grunt.registerTask('default', [
+    'jshint',
+    'concat',
+    'uglify',
+    'build-redirects'
+  ]);
 };
--- js/main.js
+++ js/main.js
@@ -1,45 +1,45 @@
-/*global _, List */
-(function( win, $ ) {
-	'use strict';
+/*global jQuery, _, List */
+(function (win, $) {
+  'use strict';
 
-	$(function() {
-		$.getJSON('http://grunt-plugin-list.herokuapp.com', function( modules ) {
-			// Only show plugins created after the specified date
-			modules = _.filter( modules, function( el ) {
-				return Date.parse( el.time.created ) > new Date('1800-01-01');
-			});
+  $(function () {
+    $.getJSON('http://grunt-plugin-list.herokuapp.com', function (modules) {
+      // only show plugins created after the specified date
+      modules = _.filter(modules, function (el) {
+        return Date.parse(el.time.created) > new Date('1800-01-01');
+      });
 
-			var latestModules = _.sortBy( modules, function( el ) {
-				return -Date.parse( el.time.created );
-			}).splice(0, 5);
+      var latestModules = _.sortBy(modules, function (el) {
+        return -Date.parse(el.time.created);
+      }).splice(0, 5);
 
-			var allModules = _.sortBy( modules, function( el ) {
-				// Remove the prefix, since not all plugins has it
-				return el.name.replace('grunt-', '');
-			});
+      var allModules = _.sortBy(modules, function (el) {
+        // removing `grunt-` since some plugins don't contain it
+        return el.name.replace('grunt-', '');
+      });
 
-			var latestTpl = _.template( $('#plugins-latest-template').html(), {
-				modules: latestModules
-			});
+      var latestTpl = _.template($('#plugins-latest-template').html(), {
+        modules: latestModules
+      });
 
-			var allTpl = _.template( $('#plugins-all-template').html(), {
-				modules: allModules
-			});
+      var allTpl = _.template($('#plugins-all-template').html(), {
+        modules: allModules
+      });
 
-			$('#loading').remove();
-			$('#plugins-latest').append( latestTpl );
-			$('#plugins-all').append( allTpl ).find('.search').show();
+      $('#loading').remove();
+      $('#plugins-latest').append(latestTpl);
+      $('#plugins-all').append(allTpl).find('.search').show();
 
-			new List('plugins-all', {
-				valueNames: [
-					'name',
-					'desc',
-					'author',
-					'modified'
-				]
-			});
+      new List('plugins-all', {
+        valueNames: [
+          'name',
+          'desc',
+          'author',
+          'modified'
+        ]
+      });
 
-			$('.modified time').timeago();
-		});
-	});
-})( window, jQuery );
+      $('#plugins-all .modified time').timeago();
+    });
+  });
+})(window, jQuery);
